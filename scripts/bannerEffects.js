e36d565512dafddb50ecc41cec3a5e8ec0cf2cf6
Rodney McQuain <rodneymcquain95@gmail.com>
2018-09-28 14:12:40 -0400

    Initial Commit

---
+++ scripts/bannerEffects.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  let bannerHeader = document.getElementById("banner-header");
+  let bannerText = document.getElementById("banner-text");
+  let bannerBtn = document.getElementById("banner-btn");
+
+  setTimeout(function() {
+    bannerHeader.classList.add("banner-appear");
+  }, 750);
+
+  setTimeout(function() {
+    bannerText.classList.add("banner-appear");
+    bannerBtn.classList.add("banner-appear");
+  }, 2000);
+});
