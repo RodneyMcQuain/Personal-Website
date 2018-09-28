9dab72c09de47b70130a75f6711e49a395d32c11
Rodney McQuain <rodneymcquain95@gmail.com>
2018-09-28 01:10:45 -0400

    Updated to reflect changes to banner and about me sections
    Banner is now full page and about me now contains a picture of myself.

--- index.php
+++ index.php
@@ -35,9 +35,13 @@
   </nav>
 
   <div class="img-fluid" id="banner-img">
-    <div style="padding: 1%;">
-      <img src="assets/images/me.jpg" class="img-fluid img-circle" id="me-img">
-      <h1 class="header" style="margin-top: 10px;">Rodney McQuain</h1>
+    <div class="col-xl-12 col-md-12 col-sm-12 col-xs-12" id="banner-container">
+      <h1 id="banner-header">Rodney McQuain</h1>
+      <p id="banner-text">
+        Hello, I am a computer science major looking for an opportunity
+        to test my skills and grow as a developer.
+      </p>
+      <a id="banner-btn" class="-curved-border" href="#about-me">Learn About Me</a>
     </div>
   </div>
 
@@ -46,19 +50,21 @@
     <div class="page-header">
       <h1><span id="about-me"></span>About Me</h1>
     </div>
-    <p>
-      I recently received my AS in software development from Ivy Tech Community College and I currently attend
-      Purdue University Fort Wayne, looking to get my BS in computer science with a software engineering focus.
-      Countless hours of my time are dedicated to furthering my knowledge of programming and becoming the best
-      I can be.  I am highly self-motivated and eager to get employed in a position where I can test my skills
-      and further my growth as a software engineer.
-    </p>
-    <p>
-      Aside from programming I put my time into competitive video games.  Just like programming it requires and improves
-      my critical thinking and problem solving skills.  I am currently ranked top 5 in Indiana for
-      Super Smash Bros. Melee.  Further, it is something I am very passionate about and is a testament
-      to my dedication of being the best that I can be at whatever I put my mind to.
-    </p>
+    <div id="about-me-container">
+      <img src="assets/images/me.jpg" class="img-fluid img-rounded" id="me-img">
+      <p id="about-me-text">
+        I recently received my AS in software development from Ivy Tech Community College and I currently attend
+        Purdue University Fort Wayne, looking to get my BS in computer science with a software engineering focus.
+        Countless hours of my time are dedicated to furthering my knowledge of programming and becoming the best
+        I can be.  I am highly self-motivated and eager to get employed in a position where I can test my skills
+        and further my growth as a software engineer.
+        <br/><br/>
+        Aside from programming I put my time into competitive video games.  Just like programming it requires and improves
+        my critical thinking and problem solving skills.  I am currently ranked top 5 in Indiana for
+        Super Smash Bros. Melee.  Further, it is something I am very passionate about and is a testament
+        to my dedication of being the best that I can be at whatever I put my mind to.
+      </p>
+    </div>
   </div>
 
   <!-- Projects -->
