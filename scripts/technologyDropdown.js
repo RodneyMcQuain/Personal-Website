9253449e4f1bf24cb52128a38a8ca41a84c48033
Rodney McQuain <rodneymcquain95@gmail.com>
2018-09-28 10:29:06 -0400

    Fixed bug where it used hidden-project-row instead of hidden-technology-row

--- scripts/technologyDropdown.js
+++ scripts/technologyDropdown.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let technologyDropdown = document.getElementById("technology-dropdown");
 
   technologyDropdown.addEventListener('click', function() {
-    let rows = document.getElementsByClassName("hidden-project-row");
+    let rows = document.getElementsByClassName("hidden-technology-row");
     let rowsVisibilityValue = rows[0].style.visibility;
 
     for (let i = 0; i < rows.length; i++)
       rows[i].classList.toggle("unhidden");
 
-    if (rows[0].className == "row hidden-project-row unhidden") {
+    if (rows[0].className == "row hidden-technology-row unhidden") {
       let row2Position = document.getElementById("technology-row-2").offsetTop - 50;
       window.scrollTo(0, row2Position);
       technologyDropdown.innerHTML = '<span class="fa fa-angle-double-up"/>';
