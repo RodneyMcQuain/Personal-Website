abd0761c744ecb4bf11585e765dcd183c4f22409
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-08 23:05:22 -0500

    Added sass to css and split up watch task
    After the .scss files are coverted to css they are then minified and renamed to allCss.min.css. Split the watch task into watch:style and watch:scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const watch = require('gulp-watch');
 
 gulp.task('pack-js', function () {
-  return gulp.src(['scripts/*.js'])
+  return gulp.src('scripts/*.js')
     .pipe(concat('allJavaScript.min.js'))
     .pipe(terser())
     .pipe(gulp.dest('assets/scripts/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('assets/js/**/*.js', ['pack-js']);
+gulp.task('pack-css', function () {
+  return gulp.src('style/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCss())
+    .pipe(rename('allCss.min.css'))
+    .pipe(gulp.dest('assets/css/'));
 });
 
-gulp.task('default', gulp.parallel('watch'));
+gulp.task('watch:scripts', function() {
+  gulp.watch('scripts/*.js', gulp.series('pack-js'));
+});
+
+gulp.task('watch:style', function() {
+  gulp.watch('style/**/*.scss', gulp.series('pack-css'));
+});
+
+gulp.task('watch', gulp.parallel('watch:scripts', 'watch:styles'));
+
+gulp.task('default', gulp.series('watch'));
