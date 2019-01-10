a559a29c9695ae9b6c8b574b30b67d9c56d20a3d
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-09 22:21:33 -0500

    Added paths variable to use for tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,38 @@ const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const watch = require('gulp-watch');
 
+const paths = {
+  src: {
+    js: 'scripts/*.js',
+    css: 'style/**/*.scss'
+  },
+  dest {
+    js: 'assets/scripts/js/',
+    css: 'assets/css/'
+  }
+}
+
 gulp.task('pack-js', function () {
-  return gulp.src('scripts/*.js')
+  return gulp.src(paths.src.js)
     .pipe(concat('allJavaScript.min.js'))
     .pipe(terser())
-    .pipe(gulp.dest('assets/scripts/js/'));
+    .pipe(gulp.dest(paths.dest.js));
 });
 
 gulp.task('pack-css', function () {
-  return gulp.src('style/**/*.scss')
+  return gulp.src(paths.src.css)
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCss())
     .pipe(rename('allCss.min.css'))
-    .pipe(gulp.dest('assets/css/'));
+    .pipe(gulp.dest(paths.dest.css));
 });
 
 gulp.task('watch:scripts', function() {
-  gulp.watch('scripts/*.js', gulp.series('pack-js'));
+  gulp.watch(paths.src.js, gulp.series('pack-js'));
 });
 
 gulp.task('watch:style', function() {
-  gulp.watch('style/**/*.scss', gulp.series('pack-css'));
+  gulp.watch(paths.src.css, gulp.series('pack-css'));
 });
 
 gulp.task('watch', gulp.parallel('watch:scripts', 'watch:style'));
