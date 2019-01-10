00d11836ddd89d45768f39bca6a65ce1304412f5
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-09 22:23:35 -0500

    Moved the scroll event into its own function

--- scripts/onScrollContainerEvent.js
+++ scripts/onScrollContainerEvent.js
@@ -1,21 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let containers = document.getElementsByClassName("container");
 
-  window.addEventListener("scroll", function(event) {
-      Array.from(containers).forEach(function(container) {
-          if (isElementInViewport(container)) {
-            container.classList.add("in-view-appear");
-          } else {
-            container.classList.remove("in-view-appear");
-          }
-      });
-  });
+  window.addEventListener("scroll", function() { scrollContainerEvent(containers) });
 
   /* Manually fired for when someone refreshes the page
   so the scroll event does not need to be triggered to
   show the content. */
   fireScrollEvent();
 });
+
+function scrollContainerEvent(containers) {
+  Array.from(containers).forEach(function(container) {
+    if (isElementInViewport(container)) {
+      container.classList.add("in-view-appear");
+    } else {
+      container.classList.remove("in-view-appear");
+    }
+  });
+}
 
 function isElementInViewport (element) {
   let rect = element.getBoundingClientRect();
