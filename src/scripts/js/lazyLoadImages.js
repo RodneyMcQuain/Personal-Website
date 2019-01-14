9760318fb7b085b9e84dba260d13d81f012b7d41
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-14 17:51:33 -0500

    Initial Commit

---
+++ src/scripts/js/lazyLoadImages.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    let images = document.getElementsByTagName('img');
+
+    for (let i = 0; i < images.length; i++) {
+      let dataSrc = images[i].getAttribute('data-src');
+
+        if (dataSrc) {
+            images[i].setAttribute('src', dataSrc);
+            images[i].classList.add("appear");
+        }
+    }
+}, false);
