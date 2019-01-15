ec3f4bf6227c7ef733dd9e391b8cb6d5cbb31034
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-14 20:17:00 -0500

    Removed addition of appear transition css class

--- src/scripts/js/lazyLoadImages.js
+++ src/scripts/js/lazyLoadImages.js
@@ -1,12 +1,11 @@
-window.addEventListener('load', function() {
-    let images = document.getElementsByTagName('img');
+window.addEventListener("load", function() {
+  let images = document.getElementsByTagName("img");
 
-    for (let i = 0; i < images.length; i++) {
-      let dataSrc = images[i].getAttribute('data-src');
+  for (let i = 0; i < images.length; i++) {
+    let dataSrc = images[i].getAttribute("data-src");
 
-        if (dataSrc) {
-            images[i].setAttribute('src', dataSrc);
-            images[i].classList.add("appear");
-        }
+    if (dataSrc) {
+      images[i].setAttribute("src", dataSrc);
     }
+  }
 }, false);
