1b22512ad108fc98ee1dc1e38b2d4be4caa2670a
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-23 09:50:43 -0500

    Moved function declarations into DOMContentLoaded event

--- src/scripts/js/onScrollContainerEvent.js
+++ src/scripts/js/onScrollContainerEvent.js
@@ -7,27 +7,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   so the scroll event does not need to be triggered to
   show the content. */
   fireScrollEvent();
+
+  function scrollContainerEvent(containers) {
+    Array.from(containers).forEach(function(container) {
+      if (isElementInViewport(container)) {
+        container.classList.add("in-view-appear");
+      } else {
+        container.classList.remove("in-view-appear");
+      }
+    });
+  }
+
+  function isElementInViewport (element) {
+    let rect = element.getBoundingClientRect();
+
+    return (
+      (rect.top > element.offsetHeight * -1) &&
+      (rect.top < window.innerHeight)
+    );
+  }
+
+  function fireScrollEvent() {
+    window.dispatchEvent(new Event("scroll"));
+  }
 });
-
-function scrollContainerEvent(containers) {
-  Array.from(containers).forEach(function(container) {
-    if (isElementInViewport(container)) {
-      container.classList.add("in-view-appear");
-    } else {
-      container.classList.remove("in-view-appear");
-    }
-  });
-}
-
-function isElementInViewport (element) {
-  let rect = element.getBoundingClientRect();
-
-  return (
-    (rect.top > element.offsetHeight * -1) &&
-    (rect.top < window.innerHeight)
-  );
-}
-
-function fireScrollEvent() {
-  window.dispatchEvent(new Event("scroll"));
-}
